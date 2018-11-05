c7412375f24671ba8358dbc55198a006c4e06e30
Tom Alcorn <tdb.alcorn@gmail.com>
2018-11-05 08:37:31 -0800

    List ops.

--- generate_model.py
+++ generate_model.py
@@ -7,46 +7,46 @@ import tensorflow as tf
 from tensorflow.python.tools.freeze_graph import freeze_graph as tf_freeze_graph
 
 
+class XOR(object):
+    def model():
+        tf.reset_default_graph()
+
+        x  = tf.placeholder(tf.float32, shape=(None, 2))
+        y = tf.placeholder(tf.float32, shape=(None,))
+
+        hidden = tf.layers.dense(x, 4,
+            activation=tf.nn.relu, 
+            kernel_initializer=tf.initializers.constant([[1, 1, -1, -1], [1, -1, 1, -1]]))
+        out_l = tf.layers.dense(hidden, 1,
+            activation=tf.nn.relu,
+            kernel_initializer=tf.initializers.constant([[-1, 1, 1, -1]]))
+
+        out = tf.reshape(out_l, (-1,), name='output')
+
+        # append :output to the name of every op that is intended to be output
+        # this way I can get what I need
+        output_names = ['output']
+
+        loss = tf.losses.mean_squared_error(y, out)
+
+        train = tf.train.AdamOptimizer(learning_rate=1e-3).minimize(loss)
+
+        return x, y, out, train, loss, output_names
+
+    def generate_batch(n=10, low=-1, high=1):
+        x = list()
+        y = list()
+        for i in range(n):
+            datum = (random.uniform(low, high), random.uniform(low, high))
+            xor = (datum[0] > 0) ^ (datum[1] > 0)
+            xor_f = 1.0 if xor else 0.0
+            x.append(datum)
+            y.append(xor_f)
+        return x, y
+
+
 def here(filename):
     return os.path.join(os.curdir, filename)
-
-
-def model():
-    tf.reset_default_graph()
-
-    x  = tf.placeholder(tf.float32, shape=(None, 2))
-    y = tf.placeholder(tf.float32, shape=(None,))
-
-    hidden = tf.layers.dense(x, 4,
-        activation=tf.nn.relu, 
-        kernel_initializer=tf.initializers.constant([[1, 1, -1, -1], [1, -1, 1, -1]]))
-    out_l = tf.layers.dense(hidden, 1,
-        activation=tf.nn.relu,
-        kernel_initializer=tf.initializers.constant([[-1, 1, 1, -1]]))
-
-    out = tf.reshape(out_l, (-1,), name='output')
-
-    # append :output to the name of every op that is intended to be output
-    # this way I can get what I need
-    output_names = ['output']
-
-    loss = tf.losses.mean_squared_error(y, out)
-
-    train = tf.train.AdamOptimizer(learning_rate=1e-3).minimize(loss)
-
-    return x, y, out, train, loss, output_names
-
-
-def generate_batch(n=10, low=-1, high=1):
-    x = list()
-    y = list()
-    for i in range(n):
-        datum = (random.uniform(low, high), random.uniform(low, high))
-        xor = (datum[0] > 0) ^ (datum[1] > 0)
-        xor_f = 1.0 if xor else 0.0
-        x.append(datum)
-        y.append(xor_f)
-    return x, y
 
 
 def save_graph(sess):
@@ -79,8 +79,26 @@ def freeze_graph(graph_def, ckpt, meta_graph, output_nodes, output_graph, input_
         meta_graph)
 
 
+def main2():
+    import sys
+    import glob
+    ckpt_path = sys.argv[1]
+    output_names = ["fco/BiasAdd"]
+    # meta_path = glob.glob(ckpt_path + '/*.meta')[0]
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(ckpt_path + '.meta')
+        saver.restore(sess, ckpt_path)
+
+        ckpt, meta_graph = save_checkpoint(sess)
+        graph_def = save_graph(sess)
+        output_graph = here("frozen.pb")
+
+        freeze_graph(graph_def, ckpt, meta_graph, output_names, output_graph)
+
+
 def main():
-    inp, label, out, train, loss, output_names = model()
+    xor = XOR()
+    inp, label, out, train, loss, output_names = xor.model()
 
     num_batches = 1000
     batch_size = 25
@@ -89,7 +107,7 @@ def main():
         sess.run(tf.global_variables_initializer())
 
         for i in range(num_batches):
-            x_batch, y_batch = generate_batch(n=batch_size, low=-1, high=1)
+            x_batch, y_batch = xor.generate_batch(n=batch_size, low=-1, high=1)
 
             _, loss_batch = sess.run([train, loss], feed_dict={
                 inp: x_batch,
@@ -108,4 +126,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main2()
--- list_ops.py
+++ list_ops.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from tensorflow.core.framework import graph_pb2
 
 
-def graph_def(pb):
+def parse_graph_def(pb):
     graph_def = graph_pb2.GraphDef()
 
     with open(pb, 'rb') as b:
@@ -13,8 +13,24 @@ def graph_def(pb):
 
     return graph_def
 
+def list_ops(graph_def):
+    return set(node.op for node in graph_def.node)
+
+def get_usages(graph_def, op):
+    return [node for node in graph_def.node if node.op == op]
+
+def get_nodes_by_name(graph_def, name_fragment):
+    return [node for node in graph_def.node if name_fragment in node.name]
 
 if __name__ == '__main__':
     import sys
 
-    main(sys.argv[1])
+    pb_file = sys.argv[1]
+    op_or_name = sys.argv[2]
+
+    graph_def = parse_graph_def(pb_file)
+
+    # print('\n'.join(list_ops(graph_def)))
+
+    print('\n'.join(map(str, get_usages(graph_def, op_or_name))))
+    # print('\n'.join(map(str, get_nodes_by_name(graph_def, op_or_name))))
